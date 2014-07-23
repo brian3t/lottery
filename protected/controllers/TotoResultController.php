fa62745ff825c8e432e1c866a418777f01dd0507
ngxtri@gmail.com <ngxtri@gmail.com>
2014-07-23 08:01:21 -0700

    Fix role admin

--- protected/controllers/TotoResultController.php
+++ protected/controllers/TotoResultController.php
@@ -36,8 +36,8 @@ class TotoResultController extends Controller
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'actions'=>array('admin','delete','admin'),
+				'roles'=>array('admin'),
 			),
 			array('deny', // deny all users
 				'users'=>array('*'),
